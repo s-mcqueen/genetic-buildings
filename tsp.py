2446b71f7cb6c1bbc3abd1528005ad7605628072
Sean McQueen <sean@mcqueen.net>
2013-04-11 00:38:35 -0700

    can generate populations

--- tsp.py
+++ tsp.py
@@ -1,6 +1,7 @@
 import math
 import random
 import webbrowser
+import copy
 
 from pygooglechart import ScatterChart
 from pygooglechart import XYLineChart
@@ -26,6 +27,7 @@ class city:
         distance = math.sqrt(pow(xd, 2) + pow(yd, 2))
         return distance
 
+
 class amap:
 
     def __init__(self, map_size):
@@ -48,26 +50,31 @@ class amap:
             print 'city %d:  (%d, %d)' % (i, c.getx(), c.gety())
             i+=1
 
+    def create_tour(self):
+        cities = copy.deepcopy(self.cities)
+        random.shuffle(cities)
+        cities.append(cities[0])
+        ms = copy.deepcopy(self.map_size)
+        return tour(cities, ms)  
+
     def graph(self):
         ''' graphs the map using google graph api '''
-        ms = self.map_size
+        ms = copy.deepcopy(self.map_size)
         chart = ScatterChart(ms, ms, x_range=(0, ms), y_range=(0, ms))
-        xdata = [c.getx() for c in self.cities]
-        ydata = [c.gety() for c in self.cities]
+        cs = copy.deepcopy(self.cities)
+        xdata = [c.getx() for c in cs]
+        ydata = [c.gety() for c in cs]
         chart.add_data(xdata)
         chart.add_data(ydata)
         webbrowser.open(chart.get_url())
 
 class tour:
-    # a tour is a chromosome...
 
-    def __init__(self, amap):
+    def __init__(self, tour_order, map_size):
         ''' initially, a tour order is a random shuffle of the cities in the map 
             PLUS the first element in the tour -- we have to go back to the start '''
-        city_shuffle = amap.get_cities()
-        random.shuffle(city_shuffle)
-        self.tour_order = city_shuffle.append(city_shuffle[0])
-        self.map_size = amap.get_map_size()
+        self.tour_order = tour_order
+        self.map_size = map_size
 
     def get_order(self):
         return self.tour_order
@@ -79,18 +86,40 @@ class tour:
         fitness = sum(pair_distances)
         return fitness
 
-    def graph(self):
-        ms = self.map_size
+    def graph(self):        
+        ms = copy.deepcopy(self.map_size)
         chart = XYLineChart(ms, ms, x_range=(0, ms), y_range=(0, ms))
-        # add x coords
-        # add y coords
+        to = copy.deepcopy(self.tour_order)
+        xcoords = [c.getx() for c in to]
+        ycoords = [c.gety() for c in to]
+        chart.add_data(xcoords)
+        chart.add_data(ycoords)
 
-def xy_rect():
+        webbrowser.open(chart.get_url())
 
-    chart = XYLineChart(100,100, x_range=(0, 100), y_range=(0, 100))
-    chart.add_data([10, 80, 80, 10, 10])  # x coords
-    chart.add_data([10, 10, 90, 90, 10])  # y coords
-    webbrowser.open(chart.get_url())
+class population:
+
+    def __init__(self, size):
+        m = rand_map(10, 250)
+        p = []
+        i = 0
+        while (i < size):
+            p.append(m.create_tour())
+            i+=1
+        self.pop = p
+
+    def get_pop(self):
+        return self.pop
+
+    def fittest(self):
+        best = float('inf')
+        best_so_far = 0
+        for p in self.pop:
+            fit = p.fitness()
+            if (best > fit):
+                best = fit
+                best_so_far = p
+        return best_so_far
 
 
 def rand_map(num_cities, map_size):
@@ -101,5 +130,13 @@ def rand_map(num_cities, map_size):
         m.add_city()
         i+=1
     return m
+
+
+
+p = population(100)
+f = p.fittest()
+f.graph()
+
+
 
 
